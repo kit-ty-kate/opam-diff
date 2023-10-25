7d5a08d14e09f59a366c4d107289900d99627c02
Kate <kit-ty-kate@outlook.com>
2023-10-25 20:10:01 +0100

    Just a simple project outputing a normalised pseudo-diff between two opam files

---
+++ src/main.ml
@@ -0,0 +1,42 @@
+let rec diff l1 l2 = match l1, l2 with
+  | (k1, v1) :: xs, (k2, v2) :: ys ->
+      if String.equal k1 k2 then begin
+        let v1 = OpamPrinter.FullPos.Normalise.value v1 in
+        let v2 = OpamPrinter.FullPos.Normalise.value v2 in
+        if not (String.equal v1 v2) then begin
+          Printf.printf "- %s: %s\n+ %s: %s\n" k1 v1 k2 v2;
+        end;
+        diff xs ys
+      end else if List.exists (fun (k, _) -> String.equal k2 k) xs then begin
+        let v1 = OpamPrinter.FullPos.Normalise.value v1 in
+        Printf.printf "- %s: %s\n" k1 v1;
+        diff xs l2
+      end else if List.exists (fun (k, _) -> String.equal k1 k) ys then begin
+        let v2 = OpamPrinter.FullPos.Normalise.value v2 in
+        Printf.printf "+ %s: %s\n" k2 v2;
+        diff l1 ys
+      end else begin
+        let v1 = OpamPrinter.FullPos.Normalise.value v1 in
+        let v2 = OpamPrinter.FullPos.Normalise.value v2 in
+        Printf.printf "- %s: %s\n+ %s: %s\n" k1 v1 k2 v2;
+        diff xs ys
+      end
+  | (k, v) :: xs, [] ->
+      let v = OpamPrinter.FullPos.Normalise.value v in
+      Printf.printf "- %s: %s\n" k v;
+      diff xs []
+  | [], (k, v) :: ys ->
+      let v = OpamPrinter.FullPos.Normalise.value v in
+      Printf.printf "+ %s: %s\n" k v;
+      diff [] ys
+  | [], [] -> ()
+
+let opam_diff x y =
+  let x = OpamFile.OPAM.read (OpamFile.make (OpamFilename.of_string x)) in
+  let y = OpamFile.OPAM.read (OpamFile.make (OpamFilename.of_string y)) in
+  diff (OpamFile.OPAM.to_list x) (OpamFile.OPAM.to_list y)
+
+let () =
+  match Sys.argv with
+  | [|_; x; y|] -> opam_diff x y
+  | _ -> failwith "read the code source"
